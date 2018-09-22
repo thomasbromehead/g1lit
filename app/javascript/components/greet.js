892aebf9f9e929fbb0e9cdba293a93a4574f52a5
Thomas Bromehead <t_bromehead@yahoo.fr>
2018-09-22 19:54:28 +0200

    Worked on UI components, navbar, cards etc

---
+++ app/javascript/components/greet.js
@@ -0,0 +1,5 @@
+function greet(name){
+  return `hello ${name}`;
+}
+
+export { greet };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,5 @@
 import "bootstrap";
+import { greet } from '../components/greet';
+console.log(greet("Thomas"));
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+initUpdateNavbarOnScroll();
