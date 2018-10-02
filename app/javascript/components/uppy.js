839a86a42ed6bb3e3b115c47b4caae8abaed9db3
Thomas Bromehead <t_bromehead@yahoo.fr>
2018-10-02 11:56:50 +0200

    Issue with webpacker

--- app/javascript/components/uppy.js
+++ app/javascript/components/uppy.js
@@ -1,13 +1,53 @@
+import Uppy, { XHRUpload, DragDrop } from 'uppy'
+import '../src/uppy.css'
+
 const Uppy = require('@uppy/core')
-const XHRUpload = require('@uppy/xhr-upload')
 const Dashboard = require('@uppy/dashboard')
+const GoogleDrive = require('@uppy/google-drive')
+const Dropbox = require('@uppy/dropbox')
+const Instagram = require('@uppy/instagram')
+const Webcam = require('@uppy/webcam')
+const Tus = require('@uppy/tus')
 
-const uppy = Uppy()
-  .use(Dashboard, {
-    trigger: '#select-files'
-  })
-  .use(XHRUpload, { endpoint: 'https://api2.transloadit.com' })
+const container = document.querySelector('DashboardContainer')
 
-uppy.on('complete', (result) => {
-  console.log(`Upload complete! We’ve uploaded these files: ${result.successful}`)
-})
+const uploader = () => {
+ if(container){
+    const uppy = Uppy({
+      debug: true,
+      autoProceed: false,
+      restrictions: {
+        maxFileSize: 1000000,
+        maxNumberOfFiles: 3,
+        minNumberOfFiles: 2,
+        allowedFileTypes: ['image/*', 'video/*']
+      }
+    })
+    .use(Dashboard, {
+      trigger: '.UppyModalOpenerBtn',
+      inline: true,
+      target: '.DashboardContainer',
+      replaceTargetContent: true,
+      showProgressDetails: true,
+      note: 'Images and video only, 2–3 files, up to 1 MB',
+      height: 470,
+      metaFields: [
+        { id: 'name', name: 'Name', placeholder: 'file name' },
+        { id: 'caption', name: 'Caption', placeholder: 'describe what the image is about' }
+      ],
+      browserBackButtonClose: true
+    })
+    .use(GoogleDrive, { target: Dashboard, serverUrl: 'https://companion.uppy.io' })
+    .use(Dropbox, { target: Dashboard, serverUrl: 'https://companion.uppy.io' })
+    .use(Instagram, { target: Dashboard, serverUrl: 'https://companion.uppy.io' })
+    .use(Webcam, { target: Dashboard })
+    .use(Tus, { endpoint: 'https://master.tus.io/files/' })
+
+    uppy.on('complete', result => {
+      console.log('successful files:', result.successful)
+      console.log('failed files:', result.failed)
+    })
+  }
+}
+
+export default uploader;
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,16 @@
 import "bootstrap";
+import "path to application.css"
+;
 
 import { greet } from '../components/greet';
 console.log(greet("Thoms"));
+
 import initUpdateNavbarOnScroll from '../components/navbar';
 initUpdateNavbarOnScroll();
+
+import uploader from '../components/uppy';
+uploader();
+
 
 import sweetAlertOnClick from "../components/sweetalert";
 // sweetAlertOnClick();
