5515df36ba54b12d24f537dc0a8747ffeed8a9c7
Thomas Bromehead <t_bromehead@yahoo.fr>
2018-10-28 11:15:09 +0100

    Adding Work on Reviews so far

--- app/controllers/flat_reviews_controller.rb
+++ app/controllers/flat_reviews_controller.rb
@@ -2,18 +2,26 @@ class FlatReviewsController < ApplicationController
   before_action :set_flat
 
   def new
+    @review = FlatReview.new
   end
 
   def create
-    FlatReview.build content:params[:id, :content], user_id: current_user.id
+    @flat_review = FlatReview.build(review_params)
+    if user.signed_in?
+      flat_review.user_id = current_user.id
+      flat_review.save
+    else 
+      flash[:notice] = "Désolé, vous ne pouvez pas laisser un avis sans être connecté(e)."
+    end
 
   end
 
   def index
-    FlatReview.all
+    @reviews = FlatReview.find(params[:flat_id, :flat_review])
   end
   
   def edit
+    FlatReview.find(@flat)
   end
 
   def update
@@ -21,9 +29,13 @@ class FlatReviewsController < ApplicationController
 
   def
 
-    private
+  private
 
   def set_flat
     Flat.find(params[:id])
+  end
+
+  def review_params
+    params.require(:flat_review).permit(:content, :star_rating)
   end
 end
