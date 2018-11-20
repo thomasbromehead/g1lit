45af6d19506c0ac75647ac24d3771cb13fa890e2
Thomas Bromehead <t_bromehead@yahoo.fr>
2018-11-20 21:42:23 +0100

    feat(User)Add Gravatar fallback if image not provided on sign up :sparkles:

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,17 @@ module ApplicationHelper
     our_user = User.find(id)
     our_user.avatar_data
   end
+
   def user_pseudo(id)
     our_user = User.find(id)
     our_user.pseudo_g1
   end
+
+  def gravatar_for(user, options = { size: 80} )
+  gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+  size = options[:size]
+  gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+  image_tag(gravatar_url, alt: user.username, class: "img-circle")
+  end
+
 end
